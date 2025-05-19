0b91ffd460e0f670a45f1ed26c1b76b3f9708123
wajiha <hackrgirl787@gmail.com>
2025-05-19 16:55:43 +0500

    Modified code

--- my_file9.cpp
+++ my_file9.cpp
@@ -40,23 +40,7 @@ int main(){
     //         cout<<"you r not born yet baby "<<endl;
     //     }
        
-  
 
-        // 6:00 AM – Wake Up & Boot Up
-        // 6:30 AM – Breakfast & Dev Fuel
-        // 7:00 AM – Deep Focus: Personal Projects or Learning
-        // 9:00 AM – Prep for Work
-        // 10:00 AM – Standup & Planning
-        // 11:00 AM – Code
-        // 1:00 PM – Lunch & Tactical Reset
-        // 2:00 PM – Problem Solving / Debugging Grind
-        // 4:00 PM – PR Reviews / Documentation
-        // 5:30 PM – Wrap-Up & Sync
-        // 6:00 PM – Work Shutdown
-        // 6:30 PM – Side Hustle / Learning Block 2
-        // 7:30 PM – Dinner & Chill
-        // 8:30 PM – Reflect / Read / Unplug
-        // 10:00 PM – Sleep
 
         /******selection control structure : switch case statement********/
         switch (age)
